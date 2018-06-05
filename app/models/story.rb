36d1c5689a42db6c8b8f03c125504df162546d06
hungmi <hungmihungmi@gmail.com>
2018-06-05 21:51:30 +0800

    stories#show

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,8 @@
 class Story < ApplicationRecord
 	validates :title, presence: true
 	has_one_attached :cover
+
+	def date
+		published_on.try(:strftime, "%Y/%m/%d") || created_at.strftime("%Y/%m/%d")
+	end
 end
